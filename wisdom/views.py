4129114b2b413c29fb712d73806fb69e38830d25
Mohammed Rifas S <rifas2475@outlook.com>
2020-08-09 21:13:40 +0530

    Adding Assignment
    Signed-off-by: Mohammed Rifas S <rifas2475@outlook.com>

--- wisdom/views.py
+++ wisdom/views.py
@@ -1,8 +1,22 @@
+from io import StringIO
+
+from django.contrib import messages
 from django.shortcuts import render
 
 
 def home(request):
     if request.method == "POST":
-        pass
+        name = request.POST["name"]
+        email = request.POST["email"]
+        service_type = request.POST["service"]
+
+        csv_file = request.FILES["fileCSV"]
+
+        if not csv_file.name.endswith(".csv"):
+            messages.error(request, "THIS IS NOT A CSV FILE")
+
+        data_set = csv_file.read().decode("UTF-8")
+        io_string = StringIO(data_set)
+        next(io_string)
 
     return render(request, "index.html")
