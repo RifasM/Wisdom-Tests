267717008ffc7de2ab5e05b6ee51788c0bd00b06
Mohammed Rifas S <rifas2475@outlook.com>
2020-08-10 16:51:06 +0530

    Adding Assignment
    Signed-off-by: Mohammed Rifas S <rifas2475@outlook.com>

--- wisdom/views.py
+++ wisdom/views.py
@@ -1,3 +1,4 @@
+import csv
 from io import StringIO
 
 from django.contrib import messages
@@ -6,7 +7,7 @@ from django.shortcuts import render
 
 def home(request):
     if request.method == "POST":
-        name = request.POST["name"]
+        q_count = request.POST["q_count"]
         email = request.POST["email"]
         service_type = request.POST["service"]
 
@@ -18,5 +19,63 @@ def home(request):
         data_set = csv_file.read().decode("UTF-8")
         io_string = StringIO(data_set)
         next(io_string)
+
+        cols = ['Student No ', 'Name of Candidate',
+                'Registration', 'Grade ', 'Gender',
+                'Name of school ', 'Date of Birth ',
+                'City of Residence', 'Date and time of test',
+                'Country of Residence', 'Extra time assistance',
+                'Question No.', 'Time Spent on question (sec)',
+                'Score if correct', 'Score if incorrect',
+                'Attempt status ', 'What you marked',
+                'Correct Answer',
+                'Outcome (Correct/Incorrect/Not Attempted)',
+                'Your score']
+
+        student = {}
+
+        for row in csv.reader(io_string, delimiter=",", quotechar="|"):
+
+            if row[2] not in student:
+                student[row[2]] = {
+                    "student_num": row[0],
+                    "name": row[1],
+                    "registration": row[2],
+                    "grade": row[3],
+                    "gender": row[4],
+                    "school": row[5],
+                    "dob": row[6],
+                    "city": row[7],
+                    "test_dt": row[8],
+                    "country": row[9],
+                    "assistance": row[10],
+                    "questions": [
+                        {
+                            "qno": row[11],
+                            "time": row[12],
+                            "correct_score": row[13],
+                            "incorrect_score": row[14],
+                            "status": row[15],
+                            "answer_marked": row[16],
+                            "answer_correct": row[17],
+                            "outcome": row[18],
+                            "score": row[19]
+                        }
+                    ]
+                }
+            else:
+                student[row[2]].get("questions").append({
+                    "qno": row[11],
+                    "time": row[12],
+                    "correct_score": row[13],
+                    "incorrect_score": row[14],
+                    "status": row[15],
+                    "answer_marked": row[16],
+                    "answer_correct": row[17],
+                    "outcome": row[18],
+                    "score": row[19]
+                })
+
+        print(student)
 
     return render(request, "index.html")
